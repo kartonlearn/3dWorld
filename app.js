bc02b94b0d8798e12dba638942c3609536310c26
kartonlearn <kartoncodes@gmail.com>
2022-01-12 17:32:16 -0800

    Update render and camera for orbital controls

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 function main () {
 
-    const canvas = document.querySelector('#c')
+    const canvas = document.querySelector('#canvasElement')
     // THREE requires these, must exist => scene camera renderer
 
     ////////////////
     // CAMERA SETUP
     ////////////////
-    const fieldOfView = 50
+    const fieldOfView = 75
     //const aspectRatio = 2
     const aspectRatio = canvas.clientWidth / canvas.clientHeight
     const nearField = 0.1
     const farField = 2000
 
     const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearField, farField)
+    camera.position.z = 1
 
     ////////////////
     // RENDERER SETUP
     ////////////////
 
     const renderer = new THREE.WebGLRenderer({canvas})
-
-    const width = canvas.clientWidth
-    const height = canvas.clientHeight
-    renderer.setSize(width, height)
-    
+   
     ////////////////
     // SCENE SETUP
     ////////////////    
@@ -41,6 +38,14 @@ function main () {
     )
 
     function render () {
+        const width = canvas.clientWidth
+        const height = canvas.clientHeight
+        
+
+        camera.aspect = width / height
+        camera.updateProjectionMatrix()
+
+        renderer.setSize(width, height, false)
         renderer.render(scene, camera)
         requestAnimationFrame(render)
     }
